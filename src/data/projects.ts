ef7f47f691ae248a1f4e1e246d2d0b699646d1fb
Andre Morpa <andremorpa@gmail.com>
2020-12-03 10:53:24 +0000

    Add projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,12 +1,78 @@
 export default [
   {
-    title: 'Population Zero',
-    img: 'https://source.unsplash.com/user/willianjusten',
-    github: 'https://git.com',
-    url: 'https://site.com',
-    ribbon: 'NEW',
+    title: 'React Avançado FAQ',
+    img: '/img/projects/faq.png',
+    github: 'https://github.com/morpa/FAQ',
+    url: 'https://faq-react-avancado.vercel.app/',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptasipsam expedita unde, cupiditate eos temporibus veritatis minima etmaiores maxime mollitia saepe ab, incidunt quod, quo doloribusdelectus omnis ratione.',
-    techs: ['React', 'Next']
+      'The application has the functionality to facilitate the search for the most frequently asked questions in the Advanced React course. MDX files are used as a database for storing information',
+    techs: [
+      'TypeScript',
+      'NextJS',
+      'Styled Components',
+      'Jest',
+      'React Testing Library',
+      'Storybook',
+      'Eslint',
+      'Prettier',
+      'Husky'
+    ]
+  },
+  {
+    title: 'Beers Explorer',
+    img: '/img/projects/beers.png',
+    github: 'https://github.com/Morpa/Beer-Explorer',
+    url: 'https://beers-explorer.vercel.app/',
+    description:
+      'The application has the functionality to search through an external api, information about beers.',
+    techs: [
+      'TypeScript',
+      'NextJS',
+      'Styled Components',
+      'Jest',
+      'React Testing Library',
+      'Storybook',
+      'Eslint',
+      'Prettier',
+      'Husky'
+    ]
+  },
+  {
+    title: 'Morpa',
+    img: '/img/projects/portfolio.png',
+    github: 'https://github.com/Morpa/personal-portfolio',
+    url: 'https://morpa.vercel.app/',
+    ribbon: 'NEW',
+    description: 'My personal portfolio to show my projects',
+    techs: [
+      'TypeScript',
+      'NextJS',
+      'Styled Components',
+      'Jest',
+      'React Testing Library',
+      'Storybook',
+      'Eslint',
+      'Prettier',
+      'Husky'
+    ]
+  },
+  {
+    title: 'Won Games',
+    img: '/img/projects/won.png',
+    github: 'https://github.com/Morpa/Won-Games-Client',
+    url: 'https://won-games.vercel.app/',
+    ribbon: 'UNDER DEVELOPMENT',
+    description: 'Fictitious online game store.',
+    techs: [
+      'TypeScript',
+      'NextJS',
+      'Styled Components',
+      'Jest',
+      'React Testing Library',
+      'Storybook',
+      'Eslint',
+      'Prettier',
+      'Husky'
+    ]
   }
 ]
